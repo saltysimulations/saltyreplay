209c8146df66f50d0dd35b854328a6b908abfd5c
Ninjo <ninjomcs@gmail.com>
2020-12-15 23:01:20 +0100

    stuff, stuff, a web server and more stuff

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,35 @@
 mod aircraft_movement;
 mod simconnect_data;
 
+use actix_files as fs;
+use actix_web::{App, HttpServer};
 use simconnect;
 use simconnect_data::AircraftData;
-use std::{fs::File, fs::OpenOptions, mem::transmute_copy, thread::sleep, time::Duration};
+use std::{
+    fs::{read_to_string, File, OpenOptions},
+    mem::transmute_copy,
+    thread,
+    thread::sleep,
+    time::Duration,
+};
+use web_view::*;
+
+#[actix_web::main]
+async fn set_up_server() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(fs::Files::new("/", "src/web").show_files_listing()))
+        .bind("127.0.0.1:5000")?
+        .run()
+        .await
+}
 
 fn main() {
     let mut conn = simconnect::SimConnector::new();
     conn.connect("FSReplay");
+
+    thread::spawn(|| match set_up_server() {
+        Ok(()) => Ok(()),
+        Err(e) => Err(e),
+    });
 
     let _file = OpenOptions::new()
         .write(true)
@@ -15,10 +37,33 @@ fn main() {
         .create(true)
         .open("data.fsreplay")
         .unwrap();
+
     let mut file_to_read = File::open("data2.json").unwrap();
 
-    AircraftData::initialize_data(&mut conn);
-    AircraftData::read_from_json(&mut file_to_read, 1, &conn);
+    web_view::builder()
+        .title("FSReplay")
+        .content(Content::Url("http://127.0.0.1:5000"))
+        .size(1200, 720)
+        .resizable(false)
+        .debug(true)
+        .user_data(())
+        .invoke_handler(|_webview, arg| {
+            match arg {
+                "rec_start" => {
+                    println!("clicked the thing");
+                }
+                "rec_stop" => {
+                    println!("clicked the other thing");
+                }
+                _ => unimplemented!(),
+            };
+            Ok(())
+        })
+        .run()
+        .unwrap();
+
+    //AircraftData::initialize_data(&mut conn);
+    //AircraftData::read_from_json(&mut file_to_read, 1, &conn);
 
     loop {
         //aircraft_movement::update_aircraft(&conn, sim_data);
