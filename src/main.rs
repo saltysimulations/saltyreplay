8c317d285ceaf4f7834c9edf3158c30450099c73
Ninjo <ninjomcs@gmail.com>
2021-01-07 12:20:21 +0100

    added license header text

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+/*
+ * SaltyReplay
+ * Copyright (C) 2021 Salty Simulations
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 use msfs::sim_connect::{
     Period, SimConnect, SimConnectRecv, SIMCONNECT_OBJECT_ID_USER,
 };
@@ -94,10 +112,9 @@ fn main() {
 
     sim.request_data_on_sim_object::<AircraftData>(0, SIMCONNECT_OBJECT_ID_USER, Period::SimFrame).unwrap();
 
-    // PLAYING (for testing purposes)
+    // Playing
     AircraftData::read_from_json(&mut _file_to_read, &mut sim);
 
-    // RECORDING (for testing purposes)
     loop {
         sim.call_dispatch().unwrap();
         sleep(Duration::from_millis(10));
